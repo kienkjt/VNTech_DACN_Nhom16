62377549e451ba13c25c3111a9097b0df7377ac9
trungkien <kienn7454@gmail.com>
2025-11-10 16:40:56 +0700

    them cac dto cho cart

---
+++ src/main/java/com/nhom16/VNTech/dto/cart/CartResponseDto.java
@@ -0,0 +1,18 @@
+package com.nhom16.VNTech.dto.cart;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartResponseDto {
+    private Long cartId;
+    private Long userId;
+    List<CartItemDto> cartItems;
+    private Long totalPrice;
+    private  int quantity;
+}
